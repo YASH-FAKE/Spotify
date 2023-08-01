01163a980796ada8d4e7f49c0e8b6a8d5f28e2e0
YASH-FAKE <yashsagar.fake@gmail.com>
2023-08-01 15:53:48 +0530

    playlist some design changes.

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import image_1 from '../images/image_1.jpg'
 import {FaPlay} from 'react-icons/fa';
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import Footer from "./Footer";
+import Advertise from "./Advertise";
+import playListimg from '../images/playlist.jpg'
 
-export default function Playlist(props){
+export default function Playlist(){
     return(
-        <div className='bg-gray-500/[0.2] xs:mb-[50px] sm:ms-[0%] sm:w-full md:w-[77%] md:ms-[23%] md:mb-[0px]'>
-            <a href={props.secData.title_link}><span className='inline-block text-white text-2xl ms-[3%] font-bold hover:underline underline-offset-1'>{props.secData.title}</span></a>
-            <a href={props.secData.sub_title_link}><span className='inline-block text-gray-500 text-md pt-1 font-bold  hover:underline underline-offset-1 xs:float-right xs:me-3 sm:float-right sm:me-3  md:ms-[50%]'>{props.secData.sub_title}</span></a>
-            <div className="ms-[3%] mt-[2%] grid xs:grid-cols-1 sm:w-full sm:grid-cols-2 sm:gap-4 md:grid-cols-4 md:gap-6 md:w-[95%]">
-            {
-                props.secCardData.map((item,key)=>
-                <div key={key} className="bg-black/[0.2] rounded-md w-full mb-5 hover:bg-gray-500/[0.2] cursor-pointer">
-                    <div className='p-4 relative display_play_button'>
-                    <button className='hidden play_button text-white bg-green-500 h-[50px] w-[50px] rounded-full left-[135px] top-[150px]'><FaPlay size='40%' fill="black" className="ms-[32%]"/></button>
-                    <img src={item.img_src} className='w-[100%] h-[190px] rounded-md mb-3'/>
-                    
-                    <span className='text-white font-bold pt-5'>{item.title}</span>
-                    <p className='text-gray-500  font-semibold text-gray-300 max-w-sm'>{item.desc}</p>
-                    </div>
+        <>
+        <Sidebar/>
+        <div className='overflow-y-auto w-[100%] h-[525px]'>
+        <Header />
+        <div className='bg-gradient-to-b from-[#e87795] to-[#804252] xs:mb-[50px] sm:ms-[0%] sm:w-full md:w-[77%] md:ms-[23%] md:mb-[0px]'>
+            <div className="flex pt-[5%] ms-[3%] shadow-lg">
+                <div>
+                    <img className="h-[70%]" src={playListimg}/>
                 </div>
-                )
-            }
+                <div className="flex flex-col text-white font-bold mt-[5%]">
+                    <span className="text-[20px]">Playlist</span>
+                    <span className="text-[80px]">Hot Hits Hindi</span>
+                    <span className="text-[10px] font-semibold text-gray-900">Hottest Hindi music served here. Cover - Rocky Aur Rani Kii Prem Kahaani</span>
                 </div>
+            </div>
         </div>
+        <Footer/>
+    </div>
+    <Advertise />
+    </>
+        
     )
 }
